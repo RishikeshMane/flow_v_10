808896ec7f4d2d44c2d476a34e57e4f17ac875f1
RishikeshMane <geeksforrex@gmail.com>
2024-06-05 11:51:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ CONTAINER=container1
 # Check if container exist and delete
 if [ "$(docker ps -a | grep -c $CONTAINER)" -gt 0 ]; then
 
-    docker rm -f $CONTAINER -y
+    docker rm -f $CONTAINER 
     echo "[---- Deleted container $CONTAINER]"
 
 else
